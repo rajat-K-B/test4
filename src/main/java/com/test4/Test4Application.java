19c911bcd09f8406a18d7359bf7ae0028ed1632a
rajat-K-B <rajatkumarbhuyan9@gmail.com>
2026-07-21 11:53:19 +0530

    something added

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -9,6 +9,7 @@ public class Test4Application {
 	public static void main(String[] args) {
 		int x = 10;
 		String name = "pearlz";
+		String hunt = "psychology";
 		SpringApplication.run(Test4Application.class, args);
 	}
 
